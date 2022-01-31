3d4ed6102fe394c39b2fba2a7fad48a81bcb17f8
Rizwana777 <rizwananaaz177@gmail.com>
2022-01-31 23:26:12 +0530

    feat:list files

--- input/input.go
+++ input/input.go
@@ -2,6 +2,7 @@ package input
 
 import (
 	"file-store-service/service"
+	"os"
 	"strings"
 )
 
@@ -11,5 +12,17 @@ func Command(str string) {
 	}
 	if strings.HasPrefix(str, "store rm") {
 		service.RemoveFile(str)
+	}
+	if strings.HasPrefix(str, "store ls") {
+		service.ListFiles()
+	}
+	if strings.HasPrefix(str, "store update") {
+		service.UpdateFile(str)
+	}
+	if strings.HasPrefix(str, "store wc") {
+		service.WordCount(str)
+	}
+	if strings.HasPrefix(str, "exit") {
+		os.Exit(1)
 	}
 }
